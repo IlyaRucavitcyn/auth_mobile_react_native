a101de9473450e76557ee9d3867f1072f891be0f
ilya.rucavitcyn <ilya.rucavitcyn@cortlex.com>
2017-11-16 16:28:10 +0300

    Button component covered with tests

--- src/components/common/Button.spec.js
+++ src/components/common/Button.spec.js
@@ -1,12 +1,39 @@
 import 'react-native';
 import React from 'react';
+import { shallow } from 'enzyme';
 import { Button } from './Button';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-test('renders correctly', () => {
-    const tree = renderer.create(
-        <Button />
+test('renders snapshot correctly', () => {
+    const wrapper = shallow(
+        <Button
+            disabled={true}
+            onPress={() => { }}>
+            Testing
+        </Button>
     );
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('inner text is passed correctly', () => {
+    const wrapper = shallow(
+        <Button>Testing text</Button>
+    );
+    expect(wrapper.childAt(0).childAt(0).text())
+        .toEqual('Testing text');
+});
+
+test('disabling attribute is passed correctly', () => {
+    const wrapper = shallow(
+        <Button disabled={false}>Testing text</Button>
+    );
+    expect(wrapper.prop('disabled'))
+        .toBe(false);
+});
+
+test('onPress attribute is passed correctly', () => {
+    const wrapper = shallow(
+        <Button onPress={() => "testing result"}>Testing text</Button>
+    );
+    expect(wrapper.prop('onPress')())
+        .toBe("testing result");
 });
